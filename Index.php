3521c058c7002cf4371062839e78e91cf184ab35
Jamadosena <J.amadosena@gmail.com>
2024-09-03 22:26:50 -0500

    Prueba Azure

--- Index.php
+++ Index.php
@@ -102,7 +102,8 @@
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header class="mb-auto">
             <div>
-                <h3 class="float-md-start mb-0">Medistock</h3>
+                <img src="Application/Resources/IMG/LogoSidebarMediStock.png" class="float-md-start mb-0" alt="MediStock"
+                    width="auto" height="75" />
                 <nav class="nav nav-masthead justify-content-center float-md-end">
                     <a class="nav-link fw-bold py-1 px-0" aria-current="page" href="Application/LogIn.php">Iniciar
                         Sesion</a>
